8e50e79cd9703c7b3ba4b679fbe8f45f29fe02c6
alinamiryuk <alinamiryuk@mail.ru>
2020-09-17 12:18:58 +0300

    dependencies

---
+++ frontend/src/redux/reducer.js
@@ -0,0 +1,12 @@
+
+const initalState = [];
+
+export const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "":
+      return state
+
+    default:
+      return state;
+  }
+};
