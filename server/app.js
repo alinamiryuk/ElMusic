f760fa6061aae9c734a8a153001986a80d2b6805
alinamiryuk <alinamiryuk@mail.ru>
2020-09-18 12:14:05 +0300

    back

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const middleware = require('./middleware/index')
 const authRouter = require('./routes/auth')
-const todoRouter = require('./routes/todo')
 const app = express()
 
 middleware(app)
 
 app.use('/api/auth', authRouter)
-app.use('/api/todos', todoRouter)
 
 module.exports = app
