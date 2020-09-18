875a475b58cd2de59ff3ddb9c1d464b7663aa373
alinamiryuk <alinamiryuk@mail.ru>
2020-09-18 16:47:14 +0300

    auth,passport jwt

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,21 +1,35 @@
-module.exports = function(app) {
-  const express = require('express')
-  const mongoose = require('mongoose')
-  const morgan = require('morgan')
-  const passport = require('passport')
+module.exports = function (app) {
+	const express = require('express')
+	const mongoose = require('mongoose')
+	const morgan = require('morgan')
+	const passport = require('passport')
+	const cors = require('cors')
+	// const session = require('express-session')
 
-  mongoose.connect('mongodb://localhost:27017/todo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
+	mongoose.connect('mongodb://localhost/spotify', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
 
-  app.use(express.urlencoded({extended: true}))
-  app.use(express.json())
-  app.use(passport.initialize())
+	app.use(express.urlencoded({ extended: true }))
+	app.use(express.json())
+	app.use(passport.initialize())
+	app.use(cors())
 
+	app.use(morgan('dev'))
 
-  app.use(morgan('dev'))
+	// app.use(
+	// 	session({
+	// 		store: new FileStore(),
+	// 		key: 'user_sid',
+	// 		secret: 'anything here',
+	// 		resave: false,
+	// 		saveUninitialized: false,
+	// 		cookie: {
+	// 			expires: 6000000,
+	// 		},
+	// 	})
+	// )
 }
-
