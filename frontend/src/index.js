b3b4a1acd04146f74aefa04794a8fa786769c8d7
Alina Miryuk <64744719+alinamiryuk@users.noreply.github.com>
2020-09-19 13:07:56 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { rootReducer } from './redux/rootReducer';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootReducer } from './redux/rootReducer'
 
-// const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
   // <Provider store={store}>
