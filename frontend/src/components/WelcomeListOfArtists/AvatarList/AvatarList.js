d34db1d7e5f4ddab6bb7c659f65b6baef6a9ebb4
Boris <mesh.boris@mail.ru>
2020-09-20 16:07:23 +0300

    rele list shows in correct vision

--- frontend/src/components/WelcomeListOfArtists/AvatarList/AvatarList.js
+++ frontend/src/components/WelcomeListOfArtists/AvatarList/AvatarList.js
@@ -1,1 +1,103 @@
-export const List = [1,2,3]
+
+export const avatar = [
+  {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },
+  {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },
+  {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  }, {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  }, {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },
+  {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },
+  {
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },{
+    author: "zivert",
+    genre: "pop",
+    sub_genre: "pop-pop",
+    img: "https://e-cdns-images.dzcdn.net/images/artist/55376e1f5f999cbbef845dc9f52b2afd/242x242-000000-80-0-0.jpg",
+  },
+]
