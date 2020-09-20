d68f04c7162103c45d6d654829f05c97e25bc572
Flunt1k <chvp00@gmail.com>
2020-09-20 14:22:47 +0300

    feat: add db for users

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,9 @@
 const express = require('express')
-const passport = require('passport')
 const router = express.Router()
 const auth = require('../controllers/auth')
 router.post('/login', auth.login)
 
 router.post('/register', auth.register)
-
-router.get('/logout', auth.logout)
 
 router.get('/check', auth.check)
 
