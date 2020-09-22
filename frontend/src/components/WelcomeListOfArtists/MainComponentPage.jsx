1e9f4788f97a0c70db0f3c0de9ea5ef25ea20cda
Flunt1k <chvp00@gmail.com>
2020-09-22 10:44:27 +0300

    feat: add protected router for main application

--- frontend/src/components/WelcomeListOfArtists/MainComponentPage.jsx
+++ frontend/src/components/WelcomeListOfArtists/MainComponentPage.jsx
@@ -1,41 +1,40 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './MainComponentPage.css'
-import { useState } from 'react'
-import { ArtistImage } from './AvatarList/ArtistImage'
+import {ArtistImage} from './AvatarList/ArtistImage'
 import {fetchAuthors} from '../../redux/actionType'
 import {useDispatch, useSelector} from 'react-redux'
-import {avatar} from './AvatarList/AvatarList'
+import {Link} from 'react-router-dom'
 
 export const MainComponentPage = () => {
-	const dispatch = useDispatch()
-	const authors = useSelector(state => state.authors)
-	const [counter, setCounter] = useState(0)
-	useEffect(() => {
-		dispatch(fetchAuthors())
-	}, [])
-	return (
-		<>
-			<div className="full-screen-for-choose-artists">
-				<h3 className="full-screen-header onboarding-screen-artists-header ">
-					Помогите нам подобрать идеальный плейлист
-				</h3>
-				<div className="onboarding-screen-artists-sub-title">
-					Кто вам нравится больше всего? Выберите не менее трех исполнителей,
-					чтобы помочь нам лучше угадать ваше настроение
-				</div>
-				<div className="onboarding-screen-grid">
-					{authors.map((ava) => (
-						<ArtistImage key={ava._id} ava={ava} onCounter={setCounter} />
-					))}
-				</div>
-				{counter >= 3 ? (
-						<div className="choosenList">
-							<a href="/MainPlayList" className="btn btn-green btn-lg">
-								Продолжить
-							</a>
-						</div>
-				) : null}
-			</div>
-		</>
-	)
+  const dispatch = useDispatch()
+  const authors = useSelector(state => state.authors)
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    dispatch(fetchAuthors())
+  }, [])
+  return (
+      <>
+        <div className="full-screen-for-choose-artists">
+          <h3 className="full-screen-header onboarding-screen-artists-header ">
+            Помогите нам подобрать идеальный плейлист
+          </h3>
+          <div className="onboarding-screen-artists-sub-title">
+            Кто вам нравится больше всего? Выберите не менее трех исполнителей,
+            чтобы помочь нам лучше угадать ваше настроение
+          </div>
+          <div className="onboarding-screen-grid">
+            {authors.map((ava) => (
+                <ArtistImage key={ava._id} ava={ava} onCounter={setCounter}/>
+            ))}
+          </div>
+          {counter >= 3 ? (
+              <div className="choosenList">
+                <Link to={'/MainPlayList'} className="btn btn-green btn-lg">
+                  Продолжить
+                </Link>
+              </div>
+          ) : null}
+        </div>
+      </>
+  )
 }
--- frontend/src/components/WelcomePage/WelcomePage.jsx
+++ frontend/src/components/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,8 @@
-import { Button } from '@material-ui/core'
 import React from 'react'
 import '../../components/WelcomePage/WelcomePage.css'
 import { Albums } from './albumResult/Albums'
 import { Footer } from './Footer/Footer'
 import { OrangeSpace } from './OrangeSpace/OrangeSpace'
-import { MainComponentPage } from '../WelcomeListOfArtists/MainComponentPage'
 import { Link } from 'react-router-dom'
 
 export const WelcomePage = (params) => {
@@ -17,18 +15,15 @@ export const WelcomePage = (params) => {
 					<div className="row row-white">
 						<h2 className="h1">Looking for a music?</h2>
 						<p className="lead">Start diving to the world of new emotions.</p>
-						<a
+						<Link
 							id="segmented-desktop-launch"
 							className="btn btn-stroked-dark"
 							role="button"
-							href="/choose-artists-first"
+							to={'/MainPlayList'}
 							data-tracking='{"category": "home", "action": "play"}'
 						>
 							Launch Web Player
-							{/* <Link to ='/choose-artists-first'>
-            <MainComponentPage/>
-            </Link> */}
-						</a>
+						</Link>
 						<Albums />
 					</div>
 				</div>
