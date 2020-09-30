0e975e426740e29d487d94d74082ca8899cdd02e
alinamiryuk <alinamiryuk@mail.ru>
2020-09-30 12:00:02 +0300

    make it better

---
+++ frontend/build/precache-manifest.6e9963e21bb20b0741f6c88955b2145a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0509223e9df31dbfcf0527220146b859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6cad6e0c8720934b15d",
+    "url": "/static/css/main.f8edc7fe.chunk.css"
+  },
+  {
+    "revision": "d14ca4780f4075fd490c",
+    "url": "/static/js/2.6d935529.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6d935529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cad6e0c8720934b15d",
+    "url": "/static/js/main.1d9e6a6f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
